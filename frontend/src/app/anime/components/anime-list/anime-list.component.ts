6ec58d2b5468a4852d8b1379020c1b34569b5698
Lutenruto <pereiravazluis@gmail.com>
2023-10-11 13:28:00 +0200

    Search is done

--- frontend/src/app/anime/components/anime-list/anime-list.component.ts
+++ frontend/src/app/anime/components/anime-list/anime-list.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Anime } from '../../models/anime.model';
-import { Observable, map } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { Observable, combineLatest, map, startWith } from 'rxjs';
 import { AnimesService } from '../../services/anime.service';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-anime-list',
@@ -15,17 +15,36 @@ export class AnimeListComponent implements OnInit {
   animes$!: Observable<Anime[]>;
   loading$!: Observable<boolean>;
 
-  constructor(private route: ActivatedRoute, 
-              private animeService: AnimesService) { }
+  searchCtrl!: FormControl;
+
+  constructor(private animeService: AnimesService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
     this.initObservables();
     this.animeService.getAnimesFromServer();
   }
 
+  private initForm() {
+    this.searchCtrl = this.formBuilder.control('');
+  }
+
   private initObservables() {
     this.loading$ = this.animeService.loading$;
-    this.animes$ = this.animeService.animes$;
+    const search$ = this.searchCtrl.valueChanges.pipe(
+      startWith(this.searchCtrl.value),
+      map(value => value.toLowerCase())
+    );
+    this.animes$ = combineLatest([
+      search$,
+      this.animeService.animes$
+    ]).pipe(
+      map(([search, animes]) => animes.filter(anime => anime.title
+          .toLowerCase()
+          .includes(search as string))
+      )
+    );
   }
 
 }
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -8,10 +9,12 @@ import { MaterialModule } from './material.module';
   declarations: [],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   exports: [
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
