bb90d0c4884119027adfa26582a0dc022568128e
Lutenruto <pereiravazluis@gmail.com>
2023-10-12 14:36:53 +0200

    Suppression ligne inutile

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -21,34 +21,11 @@ export class LoginComponent {
     });
   }
 
-  // onLogin() {
-  //   if (this.loginForm.valid) {
-  //     const email = this.loginForm.get('email')?.value;
-  //     const password = this.loginForm.get('password')?.value;
-  //     this.authService.login(email,password);      
-  //   }
-  // }
   onLogin() {
     if (this.loginForm.valid) {
       const email = this.loginForm.get('email')?.value;
       const password = this.loginForm.get('password')?.value;
-      this.authService.login(email, password).subscribe(response => {
-        const token = localStorage.getItem('token');
-        const userId = localStorage.getItem('userId');
-        // Utilisez le token et le userId comme vous le souhaitez
-        console.log('Token : ', token);
-        console.log('UserId : ', userId);        
-      });
+      this.authService.login(email, password).subscribe();
     }
   }
-
-  // onSignup() {
-  //   if (this.signupForm.valid) {
-  //     const email = this.signupForm.get('email')?.value;
-  //     const password = this.signupForm.get('password')?.value;
-  //     // Appeler le service pour se connecter
-  //     this.authService.signup(email,password)
-  //   }
-  // }
-
 }
