d53e7c3b608cf098645c71e87a4f2186febb2ee1
Lutenruto <pereiravazluis@gmail.com>
2023-10-12 17:11:41 +0200

    Ajout guard link et header link affichage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'anime', loadChildren: () => import('./anime/anime.module').then(m => m.AnimeModule) },
+  { path: 'anime', loadChildren: () => import('./anime/anime.module').then(m => m.AnimeModule), canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'anime' },
 ];
 
